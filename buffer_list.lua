3133fce8d87132e59e4071141414b2c95f037d38
Nils Nordman <nino@nordman.org>
2012-03-08 13:54:05 +0100

    Make buffer list keys configurable

--- buffer_list.lua
+++ buffer_list.lua
@@ -15,7 +15,27 @@ local L = _L
 
 local M = {}
 
-local list, buffer_source
+--- The TextUI list instance used by the buffer list.
+M.list = nil
+
+--[[- The key bindings for the buffer list.
+
+You can modifiy this to customise the key bindings to your liking. The key
+bindings are passed directly to the TextUI list, so note that the
+first argument to any function will be the TextUI list itself. You can read more
+about the TextUI list's keys [here](http://nilnor.github.com/textui/docs/modules/_M.textui.list.html#keys).
+
+If you like to add a custom key binding for closing a buffer you can bind the
+@{close_buffer} function to a key of your choice. For other actions it's likely
+that you want to obtain the currently selected buffer - you can use the
+@{currently_selected_buffer} function for that.
+]]
+M.keys = {
+  cd = function(list) M.close_buffer(list) end, -- Default binding for `close buffer`
+  esc = function(list) list:close() end -- Escape closes list by default
+}
+
+local buffer_source
 
 local function shorten_home_dir(directory)
   if not directory then return end
@@ -36,7 +56,7 @@ end
 local function get_buffer_items()
   local items = {}
   for _, buffer in ipairs(buffer_source()) do
-    if list.buffer.target ~= buffer then
+    if M.list.buffer.target ~= buffer then
       local modified = buffer.dirty and '*' or ''
       items[#items + 1] = {
         buffer_title(buffer) .. modified,
@@ -54,14 +74,34 @@ local function on_selection(list, item, shift, ctrl)
   view:goto_buffer(_BUFFERS[item.buffer])
 end
 
-local function close_buffer()
+--[[- Returns the currently selected buffer in the list.
+@param list The TextUI list instance used by the buffer list. If not provided,
+then the global list is used automatically.
+@return The currently selected buffer, if any.
+@return The currently selected buffer's name, if any.
+]]
+function M.currently_selected_buffer(list)
+  list = list or M.list
+  if not list then error('`list` must be provided', 2) end
   local item = list:get_current_selection()
-  if item then
-    local name = item[1]
+  if item then return item.buffer, item[1] end
+end
+
+--[[- Closes the currently selected buffer in the buffer list.
+@param list The TextUI list instance used by the buffer list. This function
+is ordinarily invoked as the result of a key binding, and you should thus not need
+to specify this yourself. If list isn't provided, the global list is automatically
+used.
+]]
+function M.close_buffer(list)
+  list = list or M.list
+  if not list then error('`list` must be provided', 2) end
+  local sel_buffer, name = M.currently_selected_buffer(list)
+  if sel_buffer then
     gui.statusbar_text = 'Closing ' .. name .. '..'
     local current_pos = buffer.current_pos
     local current_search = list:get_current_search()
-    view:goto_buffer(_BUFFERS[item.buffer])
+    view:goto_buffer(_BUFFERS[sel_buffer])
     local closed = buffer:close()
     list.items = get_buffer_items()
     list:show()
@@ -82,15 +122,16 @@ end
 function M.show(buffers)
   buffer_source = buffers or function() return _BUFFERS end
 
-  if not list then
-    list = tui_list.new('Buffer listing')
-    list.headers = { 'Name', 'Directory' }
-    list.on_selection = on_selection
-    list.keys.cd = close_buffer
-    list.keys.esc = function() list:close() end
+  if not M.list then
+    M.list = tui_list.new('Buffer listing')
+    M.list.headers = { 'Name', 'Directory' }
+    M.list.on_selection = on_selection
+    for k, v in pairs(M.keys) do
+      M.list.keys[k] = v
+    end
   end
-  list.items = get_buffer_items()
-  list:show()
+  M.list.items = get_buffer_items()
+  M.list:show()
   gui.statusbar_text = '[Enter] = open, [Ctrl+d] = close selected buffer'
 end
 
