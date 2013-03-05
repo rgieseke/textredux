fcf0377133530db30a3e49316c3aa9f722b2b53f
Robert Gieseke <robert.gieseke@gmail.com>
2013-03-05 16:26:50 +0100

    Fix LDoc reference

--- core/gui.lua
+++ core/gui.lua
@@ -4,7 +4,7 @@ The gui module handles GUI related operations for Textredux.
 @author Nils Nordman <nino at nordman.org>
 @copyright 2012
 @license MIT (see LICENSE)
-@module _M.textredux.gui
+@module _M.textredux.core.gui
 ]]
 local M = {}
 
